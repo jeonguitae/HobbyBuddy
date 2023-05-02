9a8841f854d63384d8e677f5d7f58dc23bd3e30a
parkheejoon <vzoo8204@naver.com>
2023-05-02 14:46:50 +0900

    공지사항

---
+++ src/main/java/kr/co/hb/admin/dao/NoticeDAO.java
@@ -0,0 +1,25 @@
+package kr.co.hb.admin.dao;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import kr.co.hb.admin.dto.NoticeDTO;
+
+public interface NoticeDAO {
+	 
+		void noticeWrite(NoticeDTO dto);
+
+	   void noticeFileWrite(int idx, String oriFileName, String newFileName);
+
+	   NoticeDTO noticeDetail(String notice_idx);
+
+	   void noticeUpHit(String notice_idx);
+
+	   ArrayList<NoticeDTO> noticeList();
+
+	   int noticeUpdate(HashMap<String, String> params);
+
+	   String noticeFindFile(String notice_idx);
+
+	   int noticeDelete(String notice_idx);
+}
