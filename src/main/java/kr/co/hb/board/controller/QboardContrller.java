fb01cc7919ccbf1a3674f8426a252a8a8b7dd5ee
parkheejoon <vzoo8204@naver.com>
2023-05-11 11:14:10 +0900

    신고 처리 테스트 및 고객센터 중간저장

--- src/main/java/kr/co/hb/board/controller/QboardContrller.java
+++ src/main/java/kr/co/hb/board/controller/QboardContrller.java
@@ -2,18 +2,23 @@ package kr.co.hb.board.controller;
 
 import java.util.HashMap;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import kr.co.hb.board.service.QboardService;
 
 @Controller
 public class QboardContrller {
+	
+	Logger logger = LoggerFactory.getLogger(this.getClass());
 	
 	@Autowired QboardService service;
 
@@ -22,26 +27,13 @@ public class QboardContrller {
 				
 		return "qBoardCreate"; 
 	}
-	@RequestMapping(value="/qBoardWrite.do", method = RequestMethod.POST)	
-	   public String qBoardWrite(Model model,MultipartFile photo, @RequestParam HashMap<String, String> params) {
-	      
-		   String page = "qBoardCreate";
-		  
-		   if (params.get("question").equals("question_default")) {
-			   model.addAttribute("msg","문의 유형을 선택하세요.");
-		   }else if (params.get("qBoard_title").equals("")) {
-			   model.addAttribute("msg","문의 제목을 입력 하세요.");
-		   }else if (params.get("notice_content").equals("")) {
-			   model.addAttribute("msg","문의 내용을 입력하세요.");
-		   }else if(params.get("open_Chk").equals("")) {
-			   model.addAttribute("msg","공개 여부를 선택하세요.");
-		   }else {
-			   service.qBoardWrite(photo, params);
-			page = "redirect:/qboard.go";
-		}
-		   
-	      return page;
-	   }
+	@RequestMapping(value = "/qboard_write.ajax",method = RequestMethod.POST)
+	// ajasx형태로 주고 받기 위해 작성
+	@ResponseBody
+	public HashMap<String, Object> join(MultipartFile photo ,@RequestParam HashMap<String, String> params){
+		logger.info("정보들 컨트롤러에 도착 : {}", params);
+		return service.qboard_write(photo,params);
+	}
 	
 	
 }
